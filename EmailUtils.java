c56d4fba200ac4c7d718708462a1db231b808d9e
elanodahora <81708484+elanodahora@users.noreply.github.com>
2022-02-01 15:32:03 -0300

    Add files via upload

---
+++ EmailUtils.java
@@ -0,0 +1,40 @@
+package br.com.email;
+
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
+
+public class EmailUtils {
+	
+	private static final String HOSTNAME = "smtp.gmail.com";			
+	
+	private static final String USERNAME = "emailorigem@gmail.com";		
+						
+	private static final String PASSWORD = "senha";
+
+	private static final String EMAILORIGEM = "emailorigem@gmail.com";   	
+		
+	public static Email conectaEmail() throws EmailException {
+	
+	Email email = new SimpleEmail();
+	email.setHostName(HOSTNAME);
+	email.setSmtpPort(587);	
+	email.setAuthenticator(new DefaultAuthenticator(USERNAME,PASSWORD));		
+	//email.setDebug(true);// habilitar essa linha caso queira ver o debug do envio de email	
+	email.setStartTLSEnabled(true);
+	email.setFrom(EMAILORIGEM);
+	return email;  
+	}   
+	
+	public static void enviaEmail(Mensagem mensagem) throws EmailException {				
+		Email email = new SimpleEmail();	
+		email = conectaEmail();   	
+		email.setSubject(mensagem.getTitulo());		
+		email.setMsg(mensagem.getMensagem());		
+		email.addTo(mensagem.getDestino());				
+		email.send();		 	
+	}	
+
+}
